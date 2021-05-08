35c4c19c5d1ceddbe8dcd1a39d682069e2c12c74
AlexBedwell <abedwell20@gmail.com>
2021-05-07 23:25:30 -0700

    updated

--- app/scripts/src/storage.js
+++ app/scripts/src/storage.js
@@ -1,18 +1,17 @@
 class Store {
     constructor(storageApi) {
-        this.api = storageApi;
+    this.api = storageApi;
     }
     get() {
-        return this.api.getItem(this.key);
+    return this.api.getItem(this.key);
     }
     set(value) {
-        this.api.setItem(this.key, value);
+    this.api.setItem(this.key, value);
     }
 }
-
 export class UserStore extends Store {
     constructor(key) {
-        super(sessionStorage);
-        this.key = key;
+    super(sessionStorage);
+    this.key = key;
     }
-}
+}    
